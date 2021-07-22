ccc181993241d97a75c94dab8bdcaedc6e0fa909
Sergio Lopez Rey <sergiolopezrey@hotmail.com>
2021-07-22 12:51:36 -0300

    Agrege Registrar, falta front

--- gym/Components/login.jsx
+++ gym/Components/login.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
-import { useSelector } from 'react-redux';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { registrarUsuario } from '../Redux/sesionDucks';
 const Login = () => {
   const sesion = useSelector((state) => state.sesion);
-
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const user = {
+      name: 'sergio',
+      surname: 'lll',
+      email: 'lopez@hotmail.com',
+      password: '1231232',
+      fechaDeNacimiento: '07-22-2021',
+    };
+    dispatch(registrarUsuario(user));
+  }, []);
   return (
     <View>
-      <Text></Text>
+      <Text>FFFFF</Text>
     </View>
   );
 };
--- gym/Redux/sesionDucks.js
+++ gym/Redux/sesionDucks.js
@@ -1,10 +1,25 @@
-import Axios from 'axios';
+import axios from 'axios';
 const configDuck = {
-  nombre: 'segio',
+  sesion: {},
 };
+const REGISTRAR_USUARIO = 'REGISTRAR_USUARIO';
 export default function reducerSesion(state = configDuck, action) {
   switch (action.type) {
+    case REGISTRAR_USUARIO:
+      return { ...state, sesion: action.payload };
     default:
       return state;
   }
 }
+export const registrarUsuario = (user) => (dispatch, getState) => {
+  axios
+    .post('http://192.168.1.98:3000/api/auth', {
+      user: user,
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
