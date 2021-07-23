7115aa582a8816ee115ecacc2b5457c45f64de94
Sergio Lopez Rey <sergiolopezrey@hotmail.com>
2021-07-22 21:14:34 -0300

    jjj

--- gym/Components/login.jsx
+++ gym/Components/login.jsx
@@ -1,23 +1,43 @@
-import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
-import { registrarUsuario } from '../Redux/sesionDucks';
+import { registrarUsuario, iniciarSesion } from '../Redux/sesionDucks';
 const Login = () => {
   const sesion = useSelector((state) => state.sesion);
+  const [password, setPassword] = useState('');
+  const [mail, setMail] = useState('');
   const dispatch = useDispatch();
+  const user = {
+    name: 'sergio',
+    surname: 'lll',
+    email: 'lopez@hotmail.com',
+    password: '1231232',
+    fechaDeNacimiento: '07-22-2021',
+  };
   useEffect(() => {
-    const user = {
-      name: 'sergio',
-      surname: 'lll',
-      email: 'lopez@hotmail.com',
-      password: '1231232',
-      fechaDeNacimiento: '07-22-2021',
-    };
-    dispatch(registrarUsuario(user));
+    dispatch(iniciarSesion(user));
   }, []);
   return (
     <View>
       <Text>FFFFF</Text>
+      <TextInput
+        style={{ height: 40 }}
+        placeholder='Ingresa aqui tu mail'
+        onChangeText={(mail) => setMail(mail)}
+        defaultValue={mail}
+      />
+      <TextInput
+        style={{ height: 40 }}
+        placeholder='Ingresa aqui tu password'
+        onChangeText={(password) => setPassword(password)}
+        defaultValue={password}
+      />
+      <Button
+        onPress={() => {
+          dispatch(iniciarSesion(user));
+        }}
+        title='Iniciar sesion'
+      />
     </View>
   );
 };
--- gym/Redux/sesionDucks.js
+++ gym/Redux/sesionDucks.js
@@ -3,9 +3,12 @@ const configDuck = {
   sesion: {},
 };
 const REGISTRAR_USUARIO = 'REGISTRAR_USUARIO';
+const INICIAR_SESION = 'INICIAR_SESION';
 export default function reducerSesion(state = configDuck, action) {
   switch (action.type) {
     case REGISTRAR_USUARIO:
+      return { ...state, sesion: action.payload };
+    case INICIAR_SESION:
       return { ...state, sesion: action.payload };
     default:
       return state;
@@ -17,9 +20,24 @@ export const registrarUsuario = (user) => (dispatch, getState) => {
       user: user,
     })
     .then(function (response) {
+      dispatch({ type: REGISTRAR_USUARIO, payload: response.data });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+export const iniciarSesion = (user) => (dispatch, getState) => {
+  axios
+    .get('http://192.168.1.98:3000/api/login', {
+      user: user,
+    })
+    .then(function (response) {
+      dispatch({ type: INICIAR_SESION, payload: response.data });
+      alert(response.data);
       console.log(response);
     })
     .catch(function (error) {
       console.log(error);
+      alert(error.data);
     });
 };
