c26af601a5a9501889a66686bd1119dbb8e4538b
Sergio Lopez Rey <sergiolopezrey@hotmail.com>
2021-08-12 00:41:36 -0300

    Fixes

--- gym/Components/usuarioLogeado/elementos/diaDeRutina.jsx
+++ gym/Components/usuarioLogeado/elementos/diaDeRutina.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,TouchableOpacity } from 'react-native';
 const DiaDeRutina = (props) => {
   return (
     <View>
       <Text>{props.item.nombre}</Text>
+      <TouchableOpacity>Agregar ejercicio</TouchableOpacity>
     </View>
   );
 };
