3b51b94b3d50c569e88e0b53c8a9dfff89ff54e0
Sergio Lopez Rey <sergiolopezrey@hotmail.com>
2022-02-17 03:06:43 -0300

    Update rutinas.jsx

--- gym/Components/usuarioLogeado/elementos/rutinas.jsx
+++ gym/Components/usuarioLogeado/elementos/rutinas.jsx
@@ -36,6 +36,8 @@ const Rutina = (props) => {
   };
 
   const translateX = useSharedValue(0);
+  const timerBorrado = useSharedValue(0);
+  const timerAbrir = useSharedValue(0);
 
   //start of animation definition
   const panEvent = useAnimatedGestureHandler(
@@ -44,8 +46,24 @@ const Rutina = (props) => {
         context.translateX = translateX.value;
       },
       onActive: (event, context) => {
+        console.log(translateX.value);
         translateX.value = event.translationX + context.translateX;
+        if (translateX.value >= 100) {
+          timerAbrir.value = timerAbrir.value + 1;
+          if (timerAbrir.value > 30) {
+            timerAbrir.value = 0;
+            runOnJS(abrirRutina)();
+            translateX.value = 0;
+          }
+        } else if (translateX.value <= -100) {
+          timerBorrado.value = timerBorrado.value + 1;
+          if (timerBorrado.value > 30) {
+            timerBorrado.value = 0;
+            runOnJS(eliminar)();
+          }
+        }
       },
+
       onEnd: (event) => {
         translateX.value = withSpring(0);
         runOnJS(abrirRutina)();
@@ -67,18 +85,20 @@ const Rutina = (props) => {
           <View style={tw`flex flex-row`}>
             <View style={tw`m-auto bg-blue-700    rounded-lg p-4 `}>
               <Icon
-                style={tw`m-auto  w-8`}
+                style={tw`m-auto  w-8 mr-10`}
                 name="eye"
                 size={30}
                 color="white"
               />
             </View>
-            <View style={tw`w-full bg-white text-center rounded-lg py-4`}>
-              <Text>Nombre de la rutina: {props.item.name}</Text>
+            <View style={tw`w-full bg-white text-center rounded-lg py-4 z-10`}>
+              <Text style={tw`text-center m-0 p-0`}>
+                Nombre de la rutina: {props.item.name}
+              </Text>
             </View>
             <View style={tw`m-auto   bg-red-700 rounded-lg p-4 `}>
               <Icon
-                style={tw`m-auto  w-8`}
+                style={tw`m-auto ml-10  w-8`}
                 name="trash"
                 size={30}
                 color="white"
